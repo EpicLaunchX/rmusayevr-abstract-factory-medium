46bca29ecd00a13c380eeedddd03757e568ec8bb
Rashad Musayev <rmusayevr19@gmail.com>
2024-06-27 10:50:56 +0400

    refactor: import

--- src/pytemplate/use_case/restaurant.py
+++ src/pytemplate/use_case/restaurant.py
@@ -1,7 +1,7 @@
 from typing import Any
 
-from pytemplate.domain.models import Restaurant
 from pytemplate.service.restaurant import RestaurantService
+from src.pytemplate.domain.models import Restaurant
 
 
 def restaurant(data: dict[str, Any], registry: dict[str, type[RestaurantService]]) -> Restaurant:
